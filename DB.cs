e17e20c39bfc49e2e3c6b63f1cb7d1e82588c200
Lejla <75275686+LejlaBeqiri@users.noreply.github.com>
2021-07-24 00:45:09 +0200

    Add files via upload

--- DB.cs
+++ DB.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
-
-namespace login_and_registration
+namespace Server
 {
     class DB
     {
+
+
         // Create a connection with mysql
 
         private MySqlConnection connection = new MySqlConnection("Database=user_db;Data Source=localhost;User Id=root;Password=1234");
-        
+
         //krijo nje funksion me hap konektimin
         public void openConnection()
         {
-            if(connection.State == System.Data.ConnectionState.Closed)
+            if (connection.State == System.Data.ConnectionState.Closed)
             {
                 connection.Open();
             }
@@ -25,7 +26,7 @@ namespace login_and_registration
 
 
         //krijo nje funksion me mbyll konektimin
-        public void closeConnection() 
+        public void closeConnection()
         {
             if (connection.State == System.Data.ConnectionState.Open)
             {
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Forms1
+namespace login_and_registration
 {
     static class Program
     {
